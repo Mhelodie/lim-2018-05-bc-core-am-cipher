23095223f2a1bbb1730d12ff6b8b0bb50f984e4a
Mhelodie <mhelodie_8_07@hotmail.com>
2018-05-30 11:03:16 -0500

    cifrado descifrado html

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
 window.cipher = {
-  // ... 
+  encode : (offset,string) => {
+  offset = parseInt(offset); //Conversión de los saltos en enteros
+  let cipherEncode = ""; //  Es la variable que almacena el cifrado
+  for (i=0; i<string.length; i++) //Creando el bucle para recorrer cada letra de lo que se desea cifrar
+  { let codeAscii = string.toUpperCase().charCodeAt(i);
+    if (codeAscii === 32) {
+        let space = " ";
+      cipherEncode += space; //+= Es agregar la variable 
+    }
+    else {
+     let charNumber = codeAscii - 65;
+         charNumber = ((charNumber + parseInt(offset)) % 26 )+65;
+         cipherEncode += String.fromCharCode(charNumber);
+    }
+  }
+    return cipherEncode;
+  },
+  
+  decode : (offset,string) =>{
+      offset = parseInt(offset); //Conversión de los saltos en enteros
+      let cipherDecode = ""; //  Es la variable que almacena el cifrado
+      for (i=0; i<string.length; i++) //Creando el bucle para recorrer cada letra de lo que se desea cifrar
+      { let codeAscii = string.charCodeAt(i);
+        if (codeAscii === 32) {
+            let space = " ";
+          cipherEncode += space; //+= Es agregar la variable 
+        }
+        else {
+         let charNumber = codeAscii + 65;
+             charNumber = ((charNumber - parseInt(offset)) % 26 )+65;
+             cipherEncode += String.fromCharCode(charNumber);
+        }
+      }
+        return cipherDecode;
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-function validar (){
-    var encriptar = document.getElementById("encriptar").value;
-    if (encriptar === ""){
-        alert("El Ingrese su texto");
-    }
-}
+
+
+let btn_cifrar = document.getElementById('btnEnc');
+let offset = document.getElementById('saltos');
+let string = document.getElementById('mensaje');
+btn_cifrar.addEventListener('click', function(){
+   let output = window.cipher.encode(offset.value,string.value);
+    document.getElementById('resultado').innerHTML = output;
+})
+/*
+let btn_descifrar = document.getElementById('btnDes');
+let offset = document.getElementById('saltos');
+let string = document.getElementById('mensaje');
+btn_cifrar.addEventListener('click', function(){
+   let output = window.cipher.encode(offset.value,string.value);
+    document.getElementById('resultado').value = output;
+})
 
 let btnLim = document.getElementById('btnLim');
 btnLim.addEventListener('click', () => {
 document.getElementById('encriptar').value = "";
 document.getElementById('desencriptar').value = "";
 document.getElementById('resultado').value="";
-});
+})
+
+function validar (){
+    var encriptar = document.getElementById("mensaje").value;
+    if (encriptar === ""){
+        alert("El Ingrese su texto");
+    }
+}
+*/
