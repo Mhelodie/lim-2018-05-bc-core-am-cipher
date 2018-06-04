1a4efb3c887a91db6fcbb9f94d710638f463c0c6
Mhelodie <mhelodie_8_07@hotmail.com>
2018-06-04 12:22:38 -0500

    proyecto final

--- src/cipher.js
+++ src/cipher.js
@@ -1,35 +1,42 @@
 window.cipher = {
+  //Creación de la función encode y el parametro
   encode : (offset,string) => {
-  offset = parseInt(offset); //Conversión de los saltos en enteros
-  let cipherEncode = ""; //  Es la variable que almacena el cifrado
-  for (i=0; i<string.length; i++) //Creando el bucle para recorrer cada letra de lo que se desea cifrar
-  { let codeAscii = string.toUpperCase().charCodeAt(i);
+  //Conversión de los saltos a enteros
+  offset = parseInt(offset); 
+  //Es la variable que almacena el cifrado
+  let cipherEncode = "";
+  //Creando el bucle para capturar cada letra de lo que se desea cifrar
+  for (let i=0;  i<string.length; i++){
+   let codeAscii = string.toUpperCase().charCodeAt(i);
     if (codeAscii === 32) {
         let space = " ";
-      cipherEncode += space; //+= Es agregar la variable 
+      //+= Es  agregar la variable
+      cipherEncode += space;  
     }
     else {
      let charNumber = codeAscii - 65;
-         charNumber = ((charNumber + parseInt(offset)) % 26 )+65;
+         charNumber = ((charNumber + parseInt(offset))% 26)+65;
          cipherEncode += String.fromCharCode(charNumber);
     }
   }
     return cipherEncode;
   },
   
-  decode : (offset,string) =>{
-      offset = parseInt(offset); //Conversión de los saltos en enteros
-      let cipherDecode = ""; //  Es la variable que almacena el cifrado
-      for (i=0; i<string.length; i++) //Creando el bucle para recorrer cada letra de lo que se desea cifrar
-      { let codeAscii = string.charCodeAt(i);
+   decode : (offset,string) => {
+    //Conversión de los saltos en enteros 
+    offset = parseInt(offset); 
+      let cipherDecode = ""; //Es la variable que almacena el cifrado
+      for (let i=0; i<string.length; i++) //Creando el bucle para recorrer cada letra de lo que se desea cifrar
+      { let codeAscii = string.toUpperCase().charCodeAt(i);
         if (codeAscii === 32) {
             let space = " ";
-          cipherEncode += space; //+= Es agregar la variable 
+          cipherDecode += space; //+= Es agregar la variable 
         }
+
         else {
          let charNumber = codeAscii + 65;
-             charNumber = ((charNumber - parseInt(offset)) % 26 )+65;
-             cipherEncode += String.fromCharCode(charNumber);
+             charNumber = ((charNumber - parseInt(offset))%26)+65;
+             cipherDecode += String.fromCharCode(charNumber);
         }
       }
         return cipherDecode;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,27 @@
-
-
+// Declarar las varibles de los elementos del HTML según el ID
+let offset = document.getElementById('saltos');
+let string = document.getElementById('mensaje');
 let btn_cifrar = document.getElementById('btnEnc');
-let offset = document.getElementById('saltos');
-let string = document.getElementById('mensaje');
-btn_cifrar.addEventListener('click', function(){
-   let output = window.cipher.encode(offset.value,string.value);
-    document.getElementById('resultado').innerHTML = output;
-})
-/*
 let btn_descifrar = document.getElementById('btnDes');
-let offset = document.getElementById('saltos');
-let string = document.getElementById('mensaje');
-btn_cifrar.addEventListener('click', function(){
-   let output = window.cipher.encode(offset.value,string.value);
-    document.getElementById('resultado').value = output;
-})
-
 let btnLim = document.getElementById('btnLim');
-btnLim.addEventListener('click', () => {
-document.getElementById('encriptar').value = "";
-document.getElementById('desencriptar').value = "";
-document.getElementById('resultado').value="";
-})
 
-function validar (){
-    var encriptar = document.getElementById("mensaje").value;
-    if (encriptar === ""){
-        alert("El Ingrese su texto");
-    }
-}
-*/
+//Creación del evento cifrar.
+btn_cifrar.addEventListener('click', () => {
+  //Devuelve lo generado pora la función encode y lo guarda en la variable output.
+  let output = window.cipher.encode(offset.value,string.value);
+  //La información del output se almacena en elemento HTML que tiene como ID: resultado.
+  document.getElementById('resultado').innerHTML = output;
+});
+
+//Creación del evento descifrar.
+btn_descifrar.addEventListener('click', () => {
+  let output = window.cipher.decode(offset.value,string.value);
+  document.getElementById('resultado').innerHTML = output;
+});
+
+//Creación del evento limpiar.
+btnLim.addEventListener('click', () => {
+document.getElementById('mensaje').value = "";
+document.getElementById('saltos').value = "";
+document.getElementById('resultado').innerHTML="";
+});
